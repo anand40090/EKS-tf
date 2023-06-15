3b6b4b385a49887a9f28ed3e58a6b4c3fca7dde3
anand40090 <32446706+anand40090@users.noreply.github.com>
2023-06-15 20:35:26 +0530

    Update 0-providers.tf

--- 0-providers.tf
+++ 0-providers.tf
@@ -10,6 +10,6 @@ terraform {
 provider "aws" {
   # Configuration options
   region     = "ap-south-1"
-  access_key = "AKIAV2KW3LI7FIOWSV4Q"
-  secret_key = "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"
+  access_key = "Use Access Key"
+  secret_key = "Use secret key"
 }
